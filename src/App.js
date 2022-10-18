d00f8f5214005ab500706f471771c9668d05b971
Atikul-Islam <atikmina25@gmail.com>
2022-10-19 02:12:38 +0600

    added button toggle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 
 import './App.css';
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
+import {getAuth, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
 import app from './Firebase/firebase.init';
+import { useState } from 'react';
 
 const auth = getAuth(app)
 
 function App() {
+  const [user, setUser] = useState({})
   const provider = new GoogleAuthProvider();
+
   const handleGoogleSignIn = () =>{
     signInWithPopup(auth, provider)
     .then(result => {
       const user = result.user;
+      setUser(user);
       console.log(user);
     })
     .catch(error => {
@@ -18,9 +22,30 @@ function App() {
     })
   }
 
+  const handleSignOut = () =>{
+    signOut(auth)
+    .then( () =>{
+      setUser({});
+    })
+    .catch( () =>{
+      setUser({});
+    })
+  }
+
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      
+      {user.email && <div>
+        <img src={user.photoURL} alt="" />
+        <h2>User Name : {user.displayName}</h2>
+        <h4>Email : {user.email} <br /></h4>
+
+      </div>}
+      {user.email ?
+        <button onClick={handleSignOut}>Google Sign Out</button>
+        :
+        <button onClick={handleGoogleSignIn}>Google Sign In</button>}
+
     </div>
   );
 }
